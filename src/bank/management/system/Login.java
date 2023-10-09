c5cb3f30b511e82ca38863d52b2d18334f46ef89
Hridoy <khanh4572@gmail.com>
2023-10-09 22:42:57 +0600

    Upload File

--- src/bank/management/system/Login.java
+++ src/bank/management/system/Login.java
@@ -10,6 +10,7 @@ public class Login extends JFrame{
     Login(){
         setTitle("Automated Teller Machine");
         setLayout(null);
+        
         ImageIcon icon1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
         Image icon = icon1.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
         ImageIcon iconScale = new ImageIcon(icon);
@@ -30,7 +31,7 @@ public class Login extends JFrame{
         add(textcardID);
         
         JTextField cardIDTextField = new JTextField();
-        cardIDTextField.setBounds(300,150,200,40);
+        cardIDTextField.setBounds(300,150,200,30);
         add(cardIDTextField);
         
         //Password text
@@ -40,18 +41,42 @@ public class Login extends JFrame{
         add(textPin);
         
         JTextField pinTextField = new JTextField();
-        pinTextField.setBounds(300,200,200,40);
+        pinTextField.setBounds(300,200,200,30);
         add(pinTextField);
+        
+        //SING IN BUTTON
+        JButton logIn = new JButton("SING IN");
+        logIn.setBounds(300,250,80,30);
+        logIn.setBackground(Color.BLACK);
+        logIn.setForeground(Color.white);
+        add(logIn);
+        
+        //CLEAR BUTTON
+        JButton singUp = new JButton("SING UP");
+        singUp.setBounds(300,300,200,30);
+        singUp.setBackground(Color.BLACK);
+        singUp.setForeground(Color.white);
+        add(singUp);
+        
+        
+        //SING UP BUTTON
+        JButton clear = new JButton("CLEAR");
+        clear.setBounds(420,250,80,30);
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.white);
+        add(clear);
+        
+        
+        
         
         
         //for the background color
         getContentPane().setBackground(Color.white);
-        
-        
-        
+     
         setSize(800,500);
         setVisible(true);
         setLocation(350,250);
+        
         
     }
     public static void main(String args[]){
